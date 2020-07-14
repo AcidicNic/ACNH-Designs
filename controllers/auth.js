9d41b693d1a2d3321aac1539bd575fe5b4071b1e
acidicnic <nic4096@gmail.com>
2020-07-14 16:58:53 -0700

    heroku cant find my user model :(

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,5 @@
 const jwt = require('jsonwebtoken');
-const User = require("../models/user");
+const User = require("../../models/user");
 
 exports.getProfile = (req, res) => {
     const username = req.params.username;
