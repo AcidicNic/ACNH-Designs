176071e71667cfcc7abf8ec37e8f59660f6a284d
acidicnic <nic4096@gmail.com>
2020-07-14 17:01:45 -0700

    capitalized the u in User

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,5 @@
 const jwt = require('jsonwebtoken');
-const User = require("../models/user");
+const User = require("../models/User");
 
 exports.getProfile = (req, res) => {
     const username = req.params.username;
